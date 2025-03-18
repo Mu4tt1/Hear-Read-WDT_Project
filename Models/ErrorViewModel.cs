753538a530446140144e7143cf58b941ac7b59b3
Mu4tt1 <mu4tt1@gmail.com>
2025-03-18 23:45:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hear_Read_WDT_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
