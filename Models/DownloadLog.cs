c640f7ebd33868b3232808b47beaed7bbfa5b47b
Mu4tt1 <mu4tt1@gmail.com>
2025-03-23 05:48:06 +0200

    I've added DownloadLog&OfflineAcess

---
+++ Models/DownloadLog.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hear_Read_WDT_Project.Models
+{
+    public class DownloadLog
+    {
+        [Key]
+        public int LogId { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime DownloadDate { get; set; }
+        public string DeviceInfo { get; set; }
+        public string IpAddress { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+
+    }
+}
---
+++ Models/OfflineAccess.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hear_Read_WDT_Project.Models
+{
+    public class OfflineAccess
+    {
+        [Key]
+        public int OfflineId { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime DownloadDate { get; set; }
+        public DateTime ValidUntil { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+
+    }
+}
