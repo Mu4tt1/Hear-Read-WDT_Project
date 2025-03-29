d146ed7f816489b025f61c82b22aa9bb6404d8d5
habebakhaledd <habebakhaledd@DESKTOP-6P7FH9K>
2025-03-29 23:08:19 +0200

    add category class

---
+++ Models/Caregory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Hear_Read_WDT_Project.Models
+{
+    public class Caregory
+    {
+        [Key]
+        public int category_id { get; set; }
+
+        public string name { get; set; }
+
+        public ICollection<book_category> book_Categories { get; set; } = new List<book_caregory>();
+
+    }
+}
