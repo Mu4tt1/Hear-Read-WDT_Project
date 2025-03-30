d3ef52133d69da5e34b692185ace247695eb20bd
habebakhaledd <habebakhaledd@DESKTOP-6P7FH9K>
2025-03-30 04:52:24 +0200

    add review class

---
+++ Models/Review.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Hear_Read_WDT_Project.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Review_id { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+         
+        public int UserId { get; set; }
+
+        public int BookId { get; set; }
+
+        public DateTime Created_at { get; set; }
+
+
+        [ForeignKey("UserID")]
+        public User user { get; set; }
+
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+
+    }
+}
