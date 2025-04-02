026d54436cd77942fc101b3db50e0a766bc69124
Esraa Abd Elbary <Esraa Abd Elbary@DESKTOP-5R3BTE3>
2025-04-02 02:02:08 +0200

    Added Author class

--- Models/Author.cs
+++ Models/Author.cs
@@ -8,7 +8,7 @@ namespace Hear_Read_WDT_Project.Models
         public int AuthorId { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
-        public virtual ICollection<Book_Authorcs> Book_Authors { get; set; }
+        public virtual ICollection<Book_Authors> Book_Authors { get; set; }
     
     }
 }
