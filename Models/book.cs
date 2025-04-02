2f91f4e9a4bd3a9528b1f3950b39f6b1b35e3fc8
gannashaheen <g.ibrahim2283@su.edu.eg>
2025-04-02 18:55:28 +0200

    added book class

---
+++ Models/book.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hear_Read_WDT_Project.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        public float Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public int NarratorId { get; set; }
+        public bool IsPremiumOnly { get; set; }
+    }
+}
